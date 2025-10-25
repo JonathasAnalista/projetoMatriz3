9f32b91e070ddd02cff9d581f23567b982c26ae4
Jonathas Guilherme <jonathasguilhermeti@gmail.com>
2025-10-25 13:49:46 -0300

    Subindo projeto 3

--- js/cfcMap.js
+++ js/cfcMap.js
@@ -1,13 +1,14 @@
 ﻿(function(global){
-  // Mapeie hostnames ou prefixos de subdomínio para os dados do CFC.
+  // Mapeie hostnames ou prefixos de subdominio para os dados do CFC.
   // Exemplo: 'cfc001.seudominio.com.br': { nome: 'CFC 001', cidade: 'Cidade Exemplo' }
-  // ou use apenas o prefixo do subdomínio: 'cfc001': { ... }.
+  // ou use apenas o prefixo do subdominio: 'cfc001': { ... }.
   const hostLevelProfiles = Object.assign({
-    'www.aproveidetran.com.br': { nome: 'CFC Aprovei Detran', cidade: 'Ouro fino mg' },
+    'aproveidetran.com.br': { nome: 'CFC Aprovei Detran', cidade: 'Ouro Fino - MG' },
+    'www.aproveidetran.com.br': { nome: 'CFC Aprovei Detran', cidade: 'Ouro Fino - MG' },
   }, global.CFC_HOST_PROFILES || {});
 
   const subdomainProfiles = Object.assign({
-    'www.sapucai.aproveidetran.com.br': { nome: 'CFC Sapucai', cidade: 'Jacutinga mg' },
+    'sapucai': { nome: 'CFC Sapucai', cidade: 'Jacutinga - MG' },
   }, global.CFC_SUBDOMAIN_PROFILES || {});
 
   global.CFC_HOST_PROFILES = hostLevelProfiles;
